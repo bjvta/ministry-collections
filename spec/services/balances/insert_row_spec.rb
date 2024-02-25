44ef539a5e7c4eccc78f2234c4fe70352028c90d
Brandon Valle <bjvtamayo78@gmail.com>
2024-02-24 23:16:47 -0400

    Add the two ways to insert rows

--- spec/services/balances/insert_row_spec.rb
+++ spec/services/balances/insert_row_spec.rb
@@ -13,8 +13,26 @@ RSpec.describe Balances::InsertRow do
     end
 
     it "inserts a row when it's a fine" do
+      create(:balance)
+      fine = create(:fine, amount: 1000)
+      expect {
+        described_class.new(fine).perform
+      }.to change { Balance.count }.by(1)
+      last_balance = Balance.last
+      expect(last_balance.previous_balance).to be < last_balance.current_balance
+      expect(last_balance.income_type).to eq("fine")
     end
     it "inserts a row when it's an income with member_weekly_collection" do
+      create(:balance)
+      member = create(:member)
+      weekly_collection = create(:weekly_collection)
+      member_weekly_collection = Members::CreateWeeklyCollectionData.new(weekly_collection, [member]).call
+      expect {
+        described_class.new(member.member_weekly_collections.last).perform
+      }.to change { Balance.count }.by(1)
+      last_balance = Balance.last
+      expect(last_balance.previous_balance).to be < last_balance.current_balance
+      expect(last_balance.income_type).to eq("weekly")
     end
   end
 end
