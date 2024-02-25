cbdaff4e60a215ce47e63fbf64ed80cee43650cc
Brandon Valle <bjvtamayo78@gmail.com>
2024-02-24 22:43:51 -0400

    Add the associations on member and weekly_collection

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,5 @@
 class Member < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
+  has_many :member_weekly_collections
 end
--- app/models/weekly_collection.rb
+++ app/models/weekly_collection.rb
@@ -2,4 +2,5 @@ class WeeklyCollection < ApplicationRecord
   validates_presence_of :week_date
   validates_presence_of :amount
   validates_numericality_of :amount, greater_than_or_equal_to: 0    
+  has_many :member_weekly_collections
 end
