c9c88b02590ad0554a81ecd97a5ad6313b02e1dd
Brandon Valle <bjvtamayo78@gmail.com>
2024-02-26 17:01:40 -0400

    All the callback after the expense creation

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -4,4 +4,12 @@ class Expense < ApplicationRecord
   validates_presence_of :amount
   validates_numericality_of :amount, greater_than_or_equal_to: 0
   belongs_to :member
+
+  after_create :insert_into_balance
+
+  private
+
+  def insert_into_balance
+    Balances::InsertRow.new(self).perform
+  end
 end
--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -7,7 +7,31 @@ RSpec.describe Expense, type: :model do
     it { should validate_presence_of(:amount) }
     it { should validate_numericality_of(:amount).is_greater_than_or_equal_to(0) }
   end
+
   describe 'associations' do
     it { should belong_to(:member) }
+  end
+
+  describe 'callbacks' do
+    describe 'after_create' do
+      let(:member) { create(:member) }
+      let(:expense) { build(:expense, member: member) }
+
+      it 'calls insert_into_balance' do
+        expect(expense).to receive(:insert_into_balance)
+        expense.save!
+      end
+    end
+  end
+
+  describe 'private methods' do
+    describe '#insert_into_balance' do
+      let(:expense) { create(:expense) }
+
+      it 'calls Balances::InsertRow.new.perform' do
+        expect(Balances::InsertRow).to receive(:new).with(expense).and_return(double(perform: true))
+        expense.send(:insert_into_balance)
+      end
+    end
   end
 end
