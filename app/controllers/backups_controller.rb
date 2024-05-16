c872b08555bb6b33e5465b04dd23e2048302e2c7
Brandon Valle <bjvtamayo78@gmail.com>
2024-05-16 17:18:13 -0400

    Update the json to export

--- app/controllers/backups_controller.rb
+++ app/controllers/backups_controller.rb
@@ -1,8 +1,7 @@
-require 'uri'
-require 'fileutils'
-require 'zip'
-
 class BackupsController < ApplicationController
+  require 'json'
+  require 'fileutils'
+
   def new
   end
 
@@ -11,38 +10,27 @@ class BackupsController < ApplicationController
     backup_dir = Rails.root.join('tmp', 'backup')
     FileUtils.mkdir_p(backup_dir)
 
-    # Parse the DATABASE_URL
-    db_url = URI.parse(ENV['DATABASE_URL'])
-    db_name = db_url.path[1..-1]  # Remove leading slash
-    db_user = db_url.user
-    db_password = db_url.password
-    db_host = db_url.host
-    db_port = db_url.port || 5432
+    # Collect all records from each table and convert them to JSON
+    backup_data = {
+      members: Member.all.as_json,
+      fee_types: FeeType.all.as_json,
+      fines: Fine.all.as_json,
+      expenses: Expense.all.as_json,
+      weekly_collections: WeeklyCollection.all.as_json,
+      member_weekly_collections: MemberWeeklyCollection.all.as_json,
+      balances: Balance.all.as_json
+    }
 
-    backup_file = backup_dir.join("#{db_name}_backup.sql")
-
-    # Execute the pg_dump command
-    `PGPASSWORD=#{db_password} pg_dump -U #{db_user} -h #{db_host} -p #{db_port} #{db_name} > #{backup_file}`
-
-    # Backup files (if you have a directory for user uploads)
-    uploads_dir = Rails.root.join('public', 'uploads')
-    FileUtils.cp_r(uploads_dir, backup_dir) if Dir.exist?(uploads_dir)
-
-    # Create a zip file of the backup
-    zipfile_name = Rails.root.join('tmp', "backup_#{Time.now.strftime('%Y%m%d%H%M%S')}.zip")
-    Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
-      Dir[File.join(backup_dir, '**', '**')].each do |file|
-        zipfile.add(file.sub(backup_dir.to_s + '/', ''), file)
-      end
+    # Write the JSON data to a file
+    backup_file = backup_dir.join("backup_#{Time.now.strftime('%Y%m%d%H%M%S')}.json")
+    File.open(backup_file, 'w') do |file|
+      file.write(JSON.pretty_generate(backup_data))
     end
 
+    # Send the file to the user
+    send_file backup_file, type: 'application/json', disposition: 'attachment', filename: backup_file.basename.to_s
+
     # Clean up the temporary backup directory
-    FileUtils.rm_rf(backup_dir)
-
-    # Send the zip file to the user
-    send_file zipfile_name, type: 'application/zip', disposition: 'attachment', filename: zipfile_name.basename.to_s
-
-    # Clean up the zip file after sending
-    zipfile_name.unlink
+    # FileUtils.rm_rf(backup_dir)
   end
 end
