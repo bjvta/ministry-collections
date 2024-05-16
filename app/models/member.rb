8896399eb06f57cb2a1f9398b606930893ef679c
Brandon Valle <bjvtamayo78@gmail.com>
2024-05-15 23:03:14 -0400

    Add the debts in the report

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,13 +2,22 @@ class Member < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   has_many :member_weekly_collections
+  has_many :fines
   scope :active_and_enabled_to_pay, -> { where(active: true, enabled_to_pay: true)}
 
   def full_name
     first_name + ' ' + last_name
   end
 
-  def debt_amount
+  def deb_weekly_collections
     member_weekly_collections.unpaid.sum(:amount)
+  end
+
+  def deb_fines
+    fines.unpaid.sum(:amount)
+  end
+
+  def debt_amount
+    deb_weekly_collections + deb_fines
   end
 end
